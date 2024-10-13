562ffc679f64aa194b9ff8b05cef1ed6257c3146
mhzoe5 <159052735+mhzoe5@users.noreply.github.com>
2024-10-14 01:10:14 +0600

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -4,7 +4,7 @@ const bot = new Telegraf(TOKEN);
 const express = require("express");
 const app = express()
 app.use(express.json())
-const web_link = "https://shibtaobot.netlify.app/";
+const web_link = "";
 const community_link = "https://t.me/mhzmeg";
 
 
