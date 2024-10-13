7e767db832f5f4ef039549b3c5a7fb0aa9f18350
mhzoe5 <159052735+mhzoe5@users.noreply.github.com>
2024-10-14 00:38:08 +0600

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -4,7 +4,7 @@ const bot = new Telegraf(TOKEN);
 const express = require("express");
 const app = express()
 app.use(express.json())
-const web_link = "https://";
+const web_link = "https://app.realshibs.com/";
 const community_link = "https://t.me/mhzmeg";
 
 
