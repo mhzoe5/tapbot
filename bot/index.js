6945dc197ae17f9bf9e1f10c0796e1bd94d93118
Md Minhaz Ali <infosomoycash@gmail.com>
2024-10-14 00:17:53 +0600

    bot

---
+++ bot/index.js
@@ -0,0 +1,40 @@
+const { Telegraf, Markup } = require("telegraf");
+const TOKEN = "7269575986:AAHshxS3hVYaL4u37Nqgs3rUgsTWk3kfGjU";
+const bot = new Telegraf(TOKEN);
+const express = require("express");
+const app = express()
+app.use(express.json())
+const web_link = "https://";
+const community_link = "https://t.me/mhzmeg";
+
+
+bot.start((ctx) => {
+    const startPayload = ctx.startPayload;
+    const urlSent = `${web_link}?ref=${startPayload}`;
+    const user = ctx.message.from;
+    const userName = user.username ? `@${user.username}` : user.first_name;
+    ctx.replyWithMarkdown(`*Hey, ${userName}! I am @Forextappbot  , Welcome to CDPTap!*
+Mine CDPTap cryptocurrency easily and earn CDPTap tokens.
+
+Start mining now and be among the biggest players earning CDPtap tokens daily.
+
+Got friends, relatives, co-workers?
+Bring them all into the game.
+More squad power, more CDPtap tokens.`, {
+        reply_markup: {
+            inline_keyboard: [
+              [{ text: "👋 Start now!", web_app: { url: urlSent } }],
+              [{ text: "Crypto ", url: community_link }]
+            
+            ],
+            in: true
+        },
+    });
+  });
+
+  
+  bot.launch();
+  
+app.listen(3000, () => {
+    console.log("server is me and now running")
+})
